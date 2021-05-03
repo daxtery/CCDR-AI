c9a795b871b4457f2db412bc1bbd859dcb7fa32c
David Pinto <daxtery11@gmail.com>
2021-05-03 19:32:42 +0100

    add type to equipment

--- ccdr/models/equipment.py
+++ ccdr/models/equipment.py
@@ -4,3 +4,4 @@ from dataclasses import dataclass
 @dataclass
 class Equipment:
     area: str
+    type_: str
--- server/InMemoryDatabase.py
+++ server/InMemoryDatabase.py
@@ -2,16 +2,17 @@ from typing import Sequence
 from ccdr.models.equipment import Equipment
 
 toy_equipments = {
-    "1": Equipment(area="Desporto"),
-    "2": Equipment(area="Saúde"),
-    "3": Equipment(area="Educação"),
-    "4": Equipment(area="Social"),
-    "5": Equipment(area="Cultura"),
+    "1": Equipment(area="Desporto", type_="Estádio"),
+    "2": Equipment(area="Saúde", type_="Centro Hospitalar"),
+    "3": Equipment(area="Educação", type_="Universidade"),
+    "4": Equipment(area="Social", type_="Centro de dia"),
+    "5": Equipment(area="Cultura", type_="Teatro"),
 }
 
 
 def get_equipment_by_id(_id: str) -> Equipment:
     return toy_equipments[_id]
+
 
 def get_all_equipments():
     return toy_equipments.items()
