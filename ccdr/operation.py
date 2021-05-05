e0cf6da886329cd5bea6f140b9eef78465f120fb
David Pinto <daxtery11@gmail.com>
2021-05-05 18:00:43 +0100

    remove unused

--- ccdr/operation.py
+++
@@ -1,9 +0,0 @@
-import pickle
-from typing import List
-
-from interference.operations import Operation
-
-
-def read_operations(path: str) -> List[Operation]:
-    with open(path, 'rb') as file:
-        return pickle.load(file)
