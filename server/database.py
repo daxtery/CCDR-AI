5557115386b951c0b158856da52dbe4fb7491694
David Pinto <daxtery11@gmail.com>
2021-05-17 14:29:38 +0100

    update ids to match database ones

--- server/database.py
+++ server/database.py
@@ -1,12 +1,12 @@
 from ccdr.models.equipment import Equipment
 
 toy_equipments = {
-    "1": Equipment(area="Desporto", type_="Estádio"),
-    "2": Equipment(area="Saúde", type_="Centro Hospitalar"),
-    "3": Equipment(area="Educação", type_="Universidade"),
-    "4": Equipment(area="Educação", type_="Escola"),
-    "5": Equipment(area="Social", type_="Centro de dia"),
-    "6": Equipment(area="Cultura", type_="Teatro"),
+    "609964dc33c8033e28f0c1f9": Equipment(area="Desporto", type_="Estádio"),
+    "609964dc33c8033e28f0c1fa": Equipment(area="Saúde", type_="Centro Hospitalar"),
+    "609964dc33c8033e28f0c1fb": Equipment(area="Educação", type_="Universidade"),
+    "609964dc33c8033e28f0c1fc": Equipment(area="Educação", type_="Escola"),
+    "609964dc33c8033e28f0c1fd": Equipment(area="Social", type_="Centro de dia"),
+    "609964dc33c8033e28f0c1fe": Equipment(area="Cultura", type_="Teatro"),
 }
 
 
