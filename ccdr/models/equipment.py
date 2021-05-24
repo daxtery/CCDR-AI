c3ad07b87fa84be43295cde192b76e8eff493bdd
David Pinto <daxtery11@gmail.com>
2021-05-24 20:27:06 +0100

    finish stringify for all? equipment type + area

--- ccdr/models/equipment.py
+++ ccdr/models/equipment.py
@@ -1,12 +1,132 @@
-from dataclasses import dataclass, asdict
+from typing import List, Dict, Union
+from typing_extensions import Literal, Final
+from dataclasses import dataclass, asdict, field
+
+EquipmentArea = Literal["Social", "Cultura", "Educação", "Desporto", "Saúde"]
 
 
 @dataclass
 class Equipment:
-    area: str
+    area: EquipmentArea
     type_: str
+    extras: Dict[str, str] = field(repr=False)
 
 
-# TODO: Keep this?
+@dataclass
+class SocialEquipment(Equipment):
+    fins_lucrativos: bool
+
+    def __init__(self, type_: str, extras: Dict[str, str], fins_lucrativos: bool) -> None:
+        super().__init__("Social", type_, extras)
+
+        self.fins_lucrativos = fins_lucrativos
+
+    def __repr__(self) -> str:
+        _fins_lucrativos = "para fins lucrativos" if self.fins_lucrativos else "sem fins lucrativos"
+        return f"{_fins_lucrativos}"
+
+
+@dataclass
+class CulturalEquipment(Equipment):
+    acesso_gratuito: bool
+    mobilidade_reduzida: bool
+
+    def __init__(self, type_: str, extras: Dict[str, str], acesso_gratuito: bool, mobilidade_reduzida: bool) -> None:
+        super().__init__("Cultura", type_, extras)
+
+        self.acesso_gratuito = acesso_gratuito
+        self.mobilidade_reduzida = mobilidade_reduzida
+
+    def __repr__(self) -> str:
+        _mobilidade_reduzida = "acesso mobilidade reduzida" if self.mobilidade_reduzida else "sem acesso mobilidade reduzida"
+        _acesso_gratuito = "grátis" if self.acesso_gratuito else "pago"
+
+        return f"{_mobilidade_reduzida} {_acesso_gratuito}"
+
+
+@dataclass
+class Escola:
+    grau_ensino: str
+
+
+@dataclass
+class EducationEquipment(Equipment):
+    escolas: List[Escola]
+
+    def __init__(self, type_: str, extras: Dict[str, str], escolas: List[Escola]) -> None:
+        super().__init__("Educação", type_, extras)
+
+        self.escolas = escolas
+
+    def __repr__(self) -> str:
+        return "\n".join(map(str, self.escolas))
+
+
+@dataclass
+class SportEquipment(Equipment):
+    iluminado: bool
+    tipo_piso: str
+    mobilidade_reduzida_pratica: bool
+    mobilidade_reduzida_assistencia: bool
+
+    def __init__(
+        self,
+        type_: str,
+        extras: Dict[str, str],
+        iluminado: bool,
+        tipo_piso: str,
+        mobilidade_reduzida_pratica: bool,
+        mobilidade_reduzida_assistencia: bool,
+    ) -> None:
+        super().__init__("Desporto", type_, extras)
+
+        self.iluminado = iluminado
+        self.tipo_piso = tipo_piso
+        self.mobilidade_reduzida_pratica = mobilidade_reduzida_pratica
+        self.mobilidade_reduzida_assistencia = mobilidade_reduzida_assistencia
+
+    def __repr__(self) -> str:
+        _iluminado = "iluminado" if self.iluminado else "não iluminado"
+        _mobilidade_reduzida_pratica = "mobilidade reduzida prática" if self.mobilidade_reduzida_pratica else "não possível mobilidade reduzida prática"
+        _mobilidade_reduzida_assistencia = "mobilidade reduzida assistência" if self.mobilidade_reduzida_assistencia else "não possível mobilidade reduzida assistência"
+
+        return f"{self.type_} {self.iluminado} {_iluminado} {_mobilidade_reduzida_pratica} {_mobilidade_reduzida_assistencia}"
+
+
+@dataclass
+class HealthEquipment(Equipment):
+    def __init__(
+        self,
+        type_: str, extras: Dict[str, str],
+    ) -> None:
+        super().__init__("Saúde", type_, extras)
+
+
+@dataclass
+class HospitalHealthEquipment(HealthEquipment):
+    agrupamento_saude: str
+    centro_hospitalar: str
+    valencias: List[str]
+    especialidades: List[str]
+
+    def __init__(
+        self,
+        extras: Dict[str, str],
+        agrupamento_saude: str,
+        centro_hospitalar: str,
+        valencias: List[str],
+        especialidades: List[str],
+    ) -> None:
+        super().__init__("Saúde Hospitalar", extras)
+
+        self.agrupamento_saude = agrupamento_saude
+        self.centro_hospitalar = centro_hospitalar
+        self.valencias = valencias
+        self.especialidades = especialidades
+
+    def __repr__(self) -> str:
+        return f"{self.type_} {self.agrupamento_saude} {self.centro_hospitalar} {self.valencias} {self.especialidades}"
+
+
 def stringify(equipment: Equipment) -> str:
-    return str(asdict(equipment))
+    return str(equipment) + str(equipment.extras)
--- server/database.py
+++ server/database.py
@@ -1,9 +1,11 @@
-from typing import Any, Iterator, Sequence, Tuple
+from typing import Any, Iterator, Sequence, Tuple, Dict, cast
 from typing_extensions import Protocol
 from typing_extensions import TypedDict
-from ccdr.models.equipment import Equipment
+from ccdr.models.equipment import Equipment, EquipmentArea, SocialEquipment, SportEquipment, HealthEquipment, CulturalEquipment, EducationEquipment, HospitalHealthEquipment, Escola
 from pymongo import database, MongoClient
 from bson.objectid import ObjectId
+
+from dataclasses import asdict
 
 
 class DatabaseAccessor(Protocol):
@@ -17,13 +19,43 @@ class DatabaseAccessor(Protocol):
 
 class InMemoryDatabaseAccessor:
 
-    toy_equipments = {
-        "609964dc33c8033e28f0c1f9": Equipment(area="Desporto", type_="Estádio"),
-        "609964dc33c8033e28f0c1fa": Equipment(area="Saúde", type_="Centro Hospitalar"),
-        "609964dc33c8033e28f0c1fb": Equipment(area="Educação", type_="Universidade"),
-        "609964dc33c8033e28f0c1fc": Equipment(area="Educação", type_="Escola"),
-        "609964dc33c8033e28f0c1fd": Equipment(area="Social", type_="Centro de dia"),
-        "609964dc33c8033e28f0c1fe": Equipment(area="Cultura", type_="Teatro"),
+    toy_equipments: Dict[str, Equipment] = {
+        "609964dc33c8033e28f0c1f9": SportEquipment(
+            type_="Estádio",
+            extras={"nome": "Estádio da Luz"},
+            iluminado=True,
+            tipo_piso="terra batida",
+            mobilidade_reduzida_pratica=True,
+            mobilidade_reduzida_assistencia=True,
+        ),
+        "609964dc33c8033e28f0c1fa": HospitalHealthEquipment(
+            extras={"nome": "Centro de Saúde de Estremoz"},
+            agrupamento_saude="Centro de Saúde Estremoz",
+            centro_hospitalar="Hospital do Espírito Santo de Évora",
+            valencias=["Radiologia"],
+            especialidades=[],
+        ),
+        "609964dc33c8033e28f0c1fb": EducationEquipment(
+            type_="Universidade",
+            extras={"nome": "Universidade de Évora"},
+            escolas=[Escola("Universidade")],
+        ),
+        "609964dc33c8033e28f0c1fc": EducationEquipment(
+            type_="Escola",
+            escolas=[Escola("Secundário"), Escola("Básico")],
+            extras={"nome": "Agrupamento de Escolas Gabriel Pereira"},
+        ),
+        "609964dc33c8033e28f0c1fd": SocialEquipment(
+            type_="Centro de dia",
+            extras={"nome": "Centro Social Nossa Senhora Auxiliadora"},
+            fins_lucrativos=True,
+        ),
+        "609964dc33c8033e28f0c1fe": CulturalEquipment(
+            type_="Templo",
+            acesso_gratuito=True,
+            mobilidade_reduzida=True,
+            extras={"nome": "Templo Romano Évora"},
+        ),
     }
 
     def get_equipment_by_id(self, _id: str):
@@ -33,9 +65,10 @@ class InMemoryDatabaseAccessor:
         return InMemoryDatabaseAccessor.toy_equipments.items()
 
 
-class EquipmentDataFromDB(TypedDict):
-    area: str
+class BaseEquipmentDataFromDB(TypedDict):
+    area: EquipmentArea
     type: str
+    extras: Dict[str, str]
 
 
 class MongoDatabaseConfig(TypedDict):
@@ -49,11 +82,63 @@ class MongoDatabaseAccessor:
         self.database = config["database"]
         self.database_host = config["database_host"]
 
-    def _transfrom_equipment_data_from_db(self, data: EquipmentDataFromDB) -> Equipment:
+    def _transfrom_equipment_data_from_db(self, data: BaseEquipmentDataFromDB) -> Equipment:
         area = data["area"]
         type_ = data["type"]
+        extras = data["extras"]
 
-        return Equipment(area, type_)
+        data_ = cast(Dict[str, Any], data)
+
+        if area == "Social":
+            return SocialEquipment(
+                type_,
+                extras,
+                data_["fins lucrativos"]
+            )
+
+        if area == "Cultura":
+            return CulturalEquipment(
+                type_,
+                extras,
+                data_["acesso gratuito"],
+                data_["mobilidade reduzida"]
+            )
+
+        if area == "Educação":
+            schools = list(
+                map(
+                    lambda dto: Escola(dto["grau ensino"]),
+                    data_["escolas"]
+                )
+            )
+
+            return EducationEquipment(type_, extras, schools)
+
+        if area == "Desporto":
+            return SportEquipment(
+                type_,
+                extras,
+                data_["iluminado"],
+                data_["tipo piso"],
+                data_["mobilidade reduzida prática"],
+                data_["mobilidade reduzida assistência"],
+            )
+
+        if area == "Saúde":
+            if type_ == "Saúde Hospitalar":
+                return HospitalHealthEquipment(
+                    extras,
+                    data_["agrupamento saude"],
+                    data_["centro hospitalar"],
+                    data_["valências"],
+                    data_["especialidades"],
+                )
+
+            else:
+                return HealthEquipment(type_, extras)
+
+        else:  # We don't know what this is
+            return Equipment(area, type_, extras)
 
     def get_equipment_by_id(self, _id: str):
         collection_name = "equipment"
